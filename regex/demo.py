2daa7addf8b846b9691f336a5605e1ecbbf51a60
yuzuki <nanami2962@gmail.com>
2021-02-01 10:52:03 +0800

    so bad

--- regex/demo.py
+++
@@ -1,9 +0,0 @@
-import re
-phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
-noNewlineRegex = re.compile('.*')
-
-print(noNewlineRegex.search('Serve the public trust.\nProtect the innocent. \nUphold the law.').group())
-
-newlineRegex = re.compile('.*', re.DOTALL)
-
-print(newlineRegex.search('Serve the public trust.\nProtect the innocent. \nUphold the law.').group())
